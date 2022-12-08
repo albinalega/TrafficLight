3ecb102bd4e05c770f822c6d414b0f65a35fa395
albinalega <lega.albina@mail.ru>
2022-12-08 20:14:03 +0900

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Альбина Лега on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
